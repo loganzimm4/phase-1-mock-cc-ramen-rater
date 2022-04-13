1d68ecb95ec2d5ece486008b68ef7f783a5d125f
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-04-13 11:56:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// write your code here
+const ramen = 'http://localhost:3000/ramens';
+
+// place ramen pics in ramen-menu div
+
+fetch (ramen)
+    .then(res => res.json())
+    .then(data => renderRamenPics(data))
+
+function renderRamenPics(data) {
+    const div = document.getElementById('ramen-menu');
+    const img = document.createElement('img');
+    img.src = data[0].image;
+    div.appendChild(img);
+   
+}
+renderRamenPics();
+
+//when an image is clicked, the information of the specified ramen is displayed to the user
+
+
+
+
+
+//allow for new ramen entries, adding its picture to the div, and logging its information
